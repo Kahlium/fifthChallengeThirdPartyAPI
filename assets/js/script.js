5cb0ef1c98eeb01034defdae8ebb2c3c3aaea809
Kahlium <kahligtz@gmail.com>
2024-05-01 16:15:25 -0600

    tasks get added to local storage now

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,14 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+//Variables
+const taskName = document.getElementById('taskName')
+const taskDate = document.getElementById('taskDate')
+const taskDescription = document.getElementById('taskDescription')
+const submitButton = document.getElementById('submitButton')
 
 
+//Functions
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
 
@@ -28,31 +34,32 @@ function handleAddTask(event){
     let currentTasks = JSON.parse(localStorage.getItem('tasks') || "[]");
     const newTask = 
     {
-        taskTitle: "",
-        taskDate: "",
-        taskDescription:"",
+        taskId: generateTaskId(),
+        taskName: taskName.value,
+        taskDate: taskDate.value,
+        taskDescription: taskDescription.value,
         status: 'to-do'
     };
     
-    if (taskTitle.value === "" || taskDate.value === "" || taskDescription.value === "")
+    if (taskName.value === "" || taskDate.value === "" || taskDescription.value === "")
     {
         return;
     } else
     {
         currentTasks.push(newTask);
         localStorage.setItem("tasks", JSON.stringify(currentTasks));
-        console.log(currentTasks.value)
+        console.log(newTask)
     }
 }
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
-
+    
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
+    
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, 
@@ -61,3 +68,5 @@ $(document).ready(function () {
     renderTaskList();
 
 });
+
+submitButton.addEventListener('click', handleAddTask);
